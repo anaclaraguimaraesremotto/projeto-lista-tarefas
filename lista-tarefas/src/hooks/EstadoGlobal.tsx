ef4bd4716b5d21b104e03d79efbdf71e5901e1fe
Ana Clara <anaclaraguimaraesremotto@gmail.com>
2024-05-11 17:14:41 -0300

    adicionando persistencia de dados à lista de tarefas

--- lista-tarefas/src/hooks/EstadoGlobal.tsx
+++ lista-tarefas/src/hooks/EstadoGlobal.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState } from "react";
+import AsyncStorage from "@react-native-community/async-storage";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 
 interface Tarefa {
@@ -25,23 +26,59 @@ export const useEstadoGlobal = () => useContext(ContextoEstadoGlobal);
 export const ProvedorEstadoGlobal: React.FC<{children: React.ReactNode}> = ({children}) => {
     const [tarefas, setTarefas] = useState<Tarefa[]>([]);
 
+    const [isRecarregandoTela, setIsRecarregandoTela] = useState(true);
+
     const adicionarTarefa = (titulo: string) => {
         const novaTarefa: Tarefa = {
             id: Date.now(),
             titulo,
         };
         setTarefas([...tarefas, novaTarefa]);
+
+        salvarTarefas(tarefas);
     };
 
     const editarTarefa = (id: number, novoTitulo: string) => {
         const novasTarefas = tarefas.map(tarefa => tarefa.id === id ? {...tarefa, titulo: novoTitulo} : tarefa);
         setTarefas(novasTarefas);
+        salvarTarefas(tarefas);
     };
 
     const excluirTarefa = (id: number) => {
         const novasTarefas = tarefas.filter(tarefa => tarefa.id !== id);
         setTarefas(novasTarefas);
+        salvarTarefas(tarefas);
     };
+
+    useEffect(() => {
+        const carregarTarefas = async () => {
+            try {
+                const tarefasArmazenadas = await AsyncStorage.getItem('tarefas');
+                if (tarefasArmazenadas) {
+                    setTarefas(JSON.parse(tarefasArmazenadas))
+                }
+            } catch (error){
+                console.error(error);
+            }
+
+            setIsRecarregandoTela(false);
+        }
+        carregarTarefas();
+    }, []);
+
+    useEffect(() => {
+        salvarTarefas(tarefas);
+    }, [tarefas]);
+
+    const salvarTarefas = async (tarefas: Tarefa[]) => {
+        if (!isRecarregandoTela) {
+            try {
+                await AsyncStorage.setItem('tarefas', JSON.stringify(tarefas));
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
 
     return(
         <ContextoEstadoGlobal.Provider value={{tarefas, adicionarTarefa, editarTarefa, excluirTarefa}}>
