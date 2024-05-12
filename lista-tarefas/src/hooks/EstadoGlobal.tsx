09944d9db1a1c12bf9003221ed61249f50262705
Ana Clara <anaclaraguimaraesremotto@gmail.com>
2024-05-11 21:26:48 -0300

    Adaptando o codigo para que o frontend se comunique diretamente com o backend

--- lista-tarefas/src/hooks/EstadoGlobal.tsx
+++ lista-tarefas/src/hooks/EstadoGlobal.tsx
@@ -1,4 +1,3 @@
-import AsyncStorage from "@react-native-community/async-storage";
 import React, { createContext, useContext, useEffect, useState } from "react";
 
 
@@ -25,6 +24,104 @@ export const useEstadoGlobal = () => useContext(ContextoEstadoGlobal);
 
 export const ProvedorEstadoGlobal: React.FC<{children: React.ReactNode}> = ({children}) => {
     const [tarefas, setTarefas] = useState<Tarefa[]>([]);
+
+    const carregarTarefas = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/tarefas');
+            if (!response.ok) {
+                throw new Error('Não foi possivel carregar as tarefas')
+            }
+            const data = await response.json();
+            setTarefas(data);
+        } catch (error) {
+            console.error('Erro ao carregar as tarefas:', error)
+        }
+    }
+
+    const adicionarTarefa = async (titulo: string) => {
+        try{
+            const response = await fetch('http://localhost:3000/tarefas', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({tarefa: titulo }),
+            });
+            if (!response.ok) {
+                throw new Error('Não foi possivel adicionar a tarefa')
+            }
+            const data = await response.json();
+            console.log('Nova tarefa adicionada:', data);
+            setTarefas([...tarefas, data])
+            
+        } catch (error) {
+            console.error('Erro ao adicionar aa tarefa', error)
+            }
+        }
+
+    const adicionarTarefa = async (titulo: string) => {
+        try{
+            const response = await fetch('http://localhost:3000/tarefas', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({tarefa: titulo }),
+            });
+            if (!response.ok) {
+                throw new Error('Não foi possivel adicionar a tarefa')
+            }
+            const data = await response.json();
+            console.log('Nova tarefa adicionada:', data);
+            setTarefas([...tarefas, data])
+            
+        } catch (error) {
+            console.error('Erro ao adicionar aa tarefa', error)
+            }
+        }
+        const editarTarefa = async (id: number, novoTitulo: string) => {
+            try{
+                const response = await fetch(`http://localhost:3000/tarefas/${id}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({tarefa: novoTitulo }),
+                });
+                if (!response.ok) {
+                    throw new Error('Não foi possivel editar a tarefa')
+                }
+                console.log('Tarefa editada com sucesso');
+                
+                const novasTarefas = tarefas.map(tarefa => tarefa.id === id ? {...tarefa, titulo: novoTitulo}: tarefa)
+                setTarefas(novasTarefas)
+                
+            } catch (error) {
+                console.error('Erro ao editar a tarefa:', error)
+                }
+            }
+
+            const excluirTarefa = async (id: number) => {
+                try{
+                    const response = await fetch(`http://localhost:3000/tarefas/${id}`, {
+                        method: 'DELETE',
+                    });
+                    if (!response.ok) {
+                        throw new Error('Não foi possivel excluir a tarefa')
+                    }
+                    console.log('Tarefa excluida com sucesso');
+                    
+                    const novasTarefas = tarefas.filter(tarefa => tarefa.id !== id)
+                    setTarefas(novasTarefas)
+                    
+                } catch (error) {
+                    console.error('Erro ao excluir a tarefa:', error)
+                    }
+                }
+                useEffect(() =>{
+                    carregarTarefas();
+                }, []);
+    }
 
     const [isRecarregandoTela, setIsRecarregandoTela] = useState(true);
 
